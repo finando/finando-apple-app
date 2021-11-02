006a97c49cf562b048b2f761af4528a53177df15
Alexander Zakharov <alzak@envisage.no>
2021-11-02 01:20:15 +0100

    feat: add accounts and account view models

---
+++ Finando/Shared/ViewModels/AccountViewModel.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Combine
+
+class AccountViewModel: ObservableObject {
+    @Published private(set) var account: Account?
+    
+    init(accountId: String) {
+        getAccount(accountId: accountId)
+    }
+
+    private func getAccount(accountId: String) {
+        Network
+            .shared
+            .apollo
+            .fetch(query: constructGetAccountQuery(accountId: accountId)) { [weak self] result in
+                switch result {
+                case .success(let response):
+                    self?.account = self?.parse(data: response.data?.account)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+
+    private func constructGetAccountQuery(accountId: String) -> GetAccountQuery {
+        return GetAccountQuery(id: accountId, balanceToDate: Date().toISO8601Format())
+    }
+
+    private func parse(data: GetAccountQuery.Data.Account?) -> Account? {
+        if let budgetAccount = data?.fragments.accountFragment.asBudgetAccount {
+            return Account.BudgetAccount(BudgetAccount(account: budgetAccount))
+        }
+        
+        if let trackingAccount = data?.fragments.accountFragment.asTrackingAccount {
+            return Account.TrackingAccount(TrackingAccount(account: trackingAccount))
+        }
+        
+        return nil
+    }
+}
---
+++ Finando/Shared/ViewModels/AccountsViewModel.swift
@@ -0,0 +1,42 @@
+import Foundation
+import Combine
+
+class AccountsViewModel: ObservableObject {
+    @Published private(set) var accounts = [Account]()
+    
+    init() {
+        listAccounts()
+    }
+    
+    private func listAccounts() {
+        Network
+            .shared
+            .apollo
+            .fetch(query: constructListAccountsQuery()) { [weak self] result in
+                switch result {
+                case .success(let response):
+                    self?.accounts = self?.parse(data: response.data?.accounts) ?? []
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
+    private func constructListAccountsQuery() -> ListAccountsQuery {
+        return ListAccountsQuery(balanceToDate: Date().toISO8601Format())
+    }
+    
+    private func parse(data: [ListAccountsQuery.Data.Account]?) -> [Account] {
+        return data?.compactMap({ account in
+            if let budgetAccount = account.fragments.accountFragment.asBudgetAccount {
+                return Account.BudgetAccount(BudgetAccount(account: budgetAccount))
+            }
+            
+            if let trackingAccount = account.fragments.accountFragment.asTrackingAccount {
+                return Account.TrackingAccount(TrackingAccount(account: trackingAccount))
+            }
+            
+            return nil
+        }) ?? []
+    }
+}
