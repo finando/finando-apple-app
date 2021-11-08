7445952082dcea1510d1f5493beed630ae07a2a5
Alexander Zakharov <alzak@envisage.no>
2021-11-08 01:23:02 +0100

    feat: add more colours

--- Finando/Shared/Models/Theme.swift
+++ Finando/Shared/Models/Theme.swift
@@ -6,11 +6,31 @@ enum Theme {
             case n0 = "Neutral_N0"
             case n10 = "Neutral_N10"
             case n20 = "Neutral_N20"
+            case n30 = "Neutral_N30"
+            case n40 = "Neutral_N40"
+            case n50 = "Neutral_N50"
+            case n60 = "Neutral_N60"
+        }
+        enum blue: String {
+            case b40 = "Blue_B40"
+            case b50 = "Blue_B50"
+            case b60 = "Blue_B60"
+            case b70 = "Blue_B70"
+            case b80 = "Blue_B80"
         }
     }
 }
 
 extension Theme.color.neutral {
+    var color: Color {
+        switch self {
+        default:
+            return Color.init(self.rawValue)
+        }
+    }
+}
+
+extension Theme.color.blue {
     var color: Color {
         switch self {
         default:
