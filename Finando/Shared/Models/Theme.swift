09f81367b8b06cb3755df73df5d87554e9dc9081
Alexander Zakharov <alzak@envisage.no>
2021-11-08 01:33:25 +0100

    feat: add more colours

--- Finando/Shared/Models/Theme.swift
+++ Finando/Shared/Models/Theme.swift
@@ -18,6 +18,26 @@ enum Theme {
             case b70 = "Blue_B70"
             case b80 = "Blue_B80"
         }
+        enum green: String {
+            case g30 = "Green_B30"
+            case g40 = "Green_B40"
+            case g50 = "Green_B50"
+            case g60 = "Green_B60"
+            case g70 = "Green_B70"
+        }
+        enum red: String {
+            case r40 = "Red_R40"
+            case r50 = "Red_R50"
+            case r60 = "Red_R60"
+            case r70 = "Red_R70"
+        }
+        enum yellow: String {
+            case y30 = "Yellow_Y30"
+            case y40 = "Yellow_Y40"
+            case y50 = "Yellow_Y50"
+            case y60 = "Yellow_Y60"
+            case y70 = "Yellow_Y70"
+        }
     }
 }
 
@@ -31,6 +51,33 @@ extension Theme.color.neutral {
 }
 
 extension Theme.color.blue {
+    var color: Color {
+        switch self {
+        default:
+            return Color.init(self.rawValue)
+        }
+    }
+}
+
+extension Theme.color.green {
+    var color: Color {
+        switch self {
+        default:
+            return Color.init(self.rawValue)
+        }
+    }
+}
+
+extension Theme.color.red {
+    var color: Color {
+        switch self {
+        default:
+            return Color.init(self.rawValue)
+        }
+    }
+}
+
+extension Theme.color.yellow {
     var color: Color {
         switch self {
         default:
