c50f33b5a84629211bf27629554480869f681a77
Alexander Zakharov <alzak@envisage.no>
2021-11-08 01:23:58 +0100

    fix: use new colours and simplify interface

--- Finando/Shared/Views/Avatar.swift
+++ Finando/Shared/Views/Avatar.swift
@@ -8,43 +8,38 @@ struct Avatar: View {
 
     var body: some View {
         HStack(spacing: 0) {
-            HStack(spacing: 0) {
-                ZStack(alignment: .center) {
-                    Circle()
-                        .fill(Color.blue)
-                        .frame(width: 48, height: 48)
-                        .focusable()
-                    
-                    Text(user.initials)
-                        .font(.system(size: 20))
-                        .foregroundColor(Color.white)
-                }
-                .padding(.trailing, 16)
+            ZStack(alignment: .center) {
+                Circle()
+                    .fill(Theme.color.blue.b50.color)
+                    .frame(width: 40, height: 40)
 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text(user.firstName)
-                        .foregroundColor(.black)
-                        .font(.system(size: 18))
-                        .bold()
-                        .lineLimit(1)
-                    
-                    Text("Finance enthusiast")
-                        .foregroundColor(.blue)
-                        .font(.system(size: 12))
-                        .lineLimit(1)
-                }
-
-                Spacer()
+                Text(user.initials)
+                    .font(.title2)
+                    .foregroundColor(Theme.color.neutral.n0.color)
             }
-            .padding(8)
-            .frame(maxWidth: .infinity)
-            .background(Theme.color.neutral.n0.color)
-            .cornerRadius(8)
-            .shadow(color: isHoveringOverAvatar ? Color.black.opacity(0.15) : Color.clear, radius: 3, x: 0, y: 0)
-            .onHover(perform: { isHoveringOverAvatar = $0 }, cursor: NSCursor.pointingHand)
-            
+            .padding(.trailing, 12)
+
+            VStack(alignment: .leading, spacing: 0) {
+                Text(user.firstName)
+                    .foregroundColor(Theme.color.neutral.n60.color)
+                    .font(.title2)
+                    .bold()
+                    .lineLimit(1)
+
+                Text("Finance enthusiast")
+                    .foregroundColor(Theme.color.blue.b50.color)
+                    .font(.callout)
+                    .lineLimit(1)
+            }
+
             Spacer()
         }
+        .padding(8)
+        .frame(maxWidth: .infinity)
+        .background(Theme.color.neutral.n0.color)
+        .cornerRadius(8)
+        .shadow(color: isHoveringOverAvatar ? Theme.color.neutral.n20.color : Color.clear, radius: 3, x: 0, y: 0)
+        .onHover(perform: { isHoveringOverAvatar = $0 }, cursor: NSCursor.pointingHand)
     }
 }
 
