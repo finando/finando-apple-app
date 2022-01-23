5e177dbfdc50facafd2fb2eadcd690b769e93517
Alexander Zakharov <alzak@envisage.no>
2022-01-24 00:02:50 +0100

    feat: add new ComponentLibrary framework to the workspace

---
+++ ComponentLibrary/ComponentLibrary/ComponentLibrary.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComponentLibrary.
+FOUNDATION_EXPORT double ComponentLibraryVersionNumber;
+
+//! Project version string for ComponentLibrary.
+FOUNDATION_EXPORT const unsigned char ComponentLibraryVersionString[];
