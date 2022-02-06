09ca835180d0158c0787c662bce30bfbb9d43e81
Alexander Zakharov <alzak@envisage.no>
2022-02-06 14:10:14 +0100

    chore: clean up previews in TabBarView

--- ComponentLibrary/ComponentLibrary/Sources/TabView/TabBarView.swift
+++ ComponentLibrary/ComponentLibrary/Sources/TabView/TabBarView.swift
@@ -6,6 +6,8 @@ struct TabBarView<Key: Hashable>: View {
 
     @EnvironmentObject private var selectedTab: SelectedTab
 
+    @Environment(\.colorScheme) private var colorScheme: ColorScheme
+
     var body: some View {
         HStack(spacing: 0) {
             Group {
@@ -33,19 +35,38 @@ struct TabBarView<Key: Hashable>: View {
 }
 
 struct TabBarView_Previews: PreviewProvider {
-    static let tabs: [Tab] = [
-        Tab(key: 1, content: { selected in AnyView(Text("Tab 1").foregroundColor(selected ? .blue : .black )) }),
-        Tab(key: 2, content: { selected in AnyView(Text("Tab 2").foregroundColor(selected ? .blue : .black )) }),
-        Tab(key: 3, content: { selected in AnyView(Text("Tab 3").foregroundColor(selected ? .blue : .black )) }),
-        Tab(key: 4, content: { selected in AnyView(Text("Tab 4").foregroundColor(selected ? .blue : .black )) }),
+    static let lightModeTabs: [Tab] = [
+        Tab(key: 1, content: { selected in AnyView(Text("Tab 1").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n60 )) }),
+        Tab(key: 2, content: { selected in AnyView(Text("Tab 2").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n60 )) }),
+        Tab(key: 3, content: { selected in AnyView(Text("Tab 3").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n60 )) }),
+        Tab(key: 4, content: { selected in AnyView(Text("Tab 4").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n60 )) }),
+    ]
+    static let darkModeTabs: [Tab] = [
+        Tab(key: 1, content: { selected in AnyView(Text("Tab 1").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n0 )) }),
+        Tab(key: 2, content: { selected in AnyView(Text("Tab 2").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n0 )) }),
+        Tab(key: 3, content: { selected in AnyView(Text("Tab 3").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n0 )) }),
+        Tab(key: 4, content: { selected in AnyView(Text("Tab 4").foregroundColor(selected ? .theme.blue.b50 : .theme.neutral.n0 )) }),
     ]
     static let key: Int = 2
 
     static var previews: some View {
-        TabBarView(
-            tabs: tabs,
-            selection: .constant(key)
-        )
-            .environmentObject(SelectedTab(key: key))
+        Group {
+            TabBarView(
+                tabs: lightModeTabs,
+                selection: .constant(key)
+            )
+                .preferredColorScheme(.light)
+                .previewDisplayName("Light mode")
+
+            TabBarView(
+                tabs: darkModeTabs,
+                selection: .constant(key)
+            )
+                .preferredColorScheme(.dark)
+                .previewDisplayName("Dark mode")
+        }
+        .previewLayout(.sizeThatFits)
+        .environmentObject(SelectedTab(key: key))
+        .padding(.vertical, 16)
     }
 }
