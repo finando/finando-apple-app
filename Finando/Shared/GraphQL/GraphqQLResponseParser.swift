f0864358b3617d2be501314ab03a8eba50849fff
Alexander Zakharov <alzak@envisage.no>
2022-02-07 21:58:28 +0100

    refactor: minor simplifications

--- Finando/Shared/GraphQL/GraphqQLResponseParser.swift
+++ Finando/Shared/GraphQL/GraphqQLResponseParser.swift
@@ -2,11 +2,11 @@ enum GraphQLResponseParser {
     static func parse(data: [ListAccountsQuery.Data.Account]?) -> [Account] {
         return data?.compactMap({ account in
             if let budgetAccount = account.fragments.listAccountsAccountFragment.asBudgetAccount {
-                return Account.BudgetAccount(BudgetAccount(fragment: budgetAccount))
+                return .BudgetAccount(BudgetAccount(fragment: budgetAccount))
             }
 
             if let trackingAccount = account.fragments.listAccountsAccountFragment.asTrackingAccount {
-                return Account.TrackingAccount(TrackingAccount(fragment: trackingAccount))
+                return .TrackingAccount(TrackingAccount(fragment: trackingAccount))
             }
 
             return nil
@@ -15,11 +15,11 @@ enum GraphQLResponseParser {
 
     static func parse(data: GetAccountQuery.Data.Account?) -> Account? {
         if let budgetAccount = data?.fragments.getAccountAccountFragment.asBudgetAccount {
-            return Account.BudgetAccount(BudgetAccount(fragment: budgetAccount))
+            return .BudgetAccount(BudgetAccount(fragment: budgetAccount))
         }
 
         if let trackingAccount = data?.fragments.getAccountAccountFragment.asTrackingAccount {
-            return Account.TrackingAccount(TrackingAccount(fragment: trackingAccount))
+            return .TrackingAccount(TrackingAccount(fragment: trackingAccount))
         }
 
         return nil
@@ -27,7 +27,7 @@ enum GraphQLResponseParser {
 
     static func parse(data: CreateBudgetAccountMutation.Data.Account?) -> Account? {
         if let budgetAccount = data?.fragments.createBudgetAccountBudgetAccountFragment {
-            return Account.BudgetAccount(BudgetAccount(fragment: budgetAccount))
+            return .BudgetAccount(BudgetAccount(fragment: budgetAccount))
         }
 
         return nil
@@ -35,7 +35,7 @@ enum GraphQLResponseParser {
 
     static func parse(data: CreateTrackingAccountMutation.Data.Account?) -> Account? {
         if let trackingAccount = data?.fragments.createTrackingAccountTrackingAccountFragment {
-            return Account.TrackingAccount(TrackingAccount(fragment: trackingAccount))
+            return .TrackingAccount(TrackingAccount(fragment: trackingAccount))
         }
 
         return nil
@@ -43,7 +43,7 @@ enum GraphQLResponseParser {
 
     static func parse(data: DeleteBudgetAccountMutation.Data.Account?) -> Account? {
         if let budgetAccount = data?.fragments.deleteBudgetAccountBudgetAccountFragment {
-            return Account.BudgetAccount(BudgetAccount(fragment: budgetAccount))
+            return .BudgetAccount(BudgetAccount(fragment: budgetAccount))
         }
 
         return nil
@@ -51,7 +51,7 @@ enum GraphQLResponseParser {
 
     static func parse(data: DeleteTrackingAccountMutation.Data.Account?) -> Account? {
         if let trackingAccount = data?.fragments.deleteTrackingAccountTrackingAccountFragment {
-            return Account.TrackingAccount(TrackingAccount(fragment: trackingAccount))
+            return .TrackingAccount(TrackingAccount(fragment: trackingAccount))
         }
 
         return nil
---
+++ Finando/Shared/Models/TransactionType.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionType.swift
+//  Finando
+//
+//  Created by Alexander Zakharov on 07/02/2022.
+//
+
+import Foundation
