148a0cf38349bb407a91907a51def935ddcaea3b
Alexander Zakharov <alzak@envisage.no>
2022-02-20 16:28:34 +0100

    feat: implement confirmation dialog when deleting account

--- Finando/iOS/Views/AccountView+iOS.swift
+++ Finando/iOS/Views/AccountView+iOS.swift
@@ -6,23 +6,32 @@ struct AccountView: View {
     let account: Account
 
     @State private var showNewTransactionSheet = false
+    @State private var showDeleteAccountConfirmationDialog = false
 
     var body: some View {
         WithViewStore(store) { viewStore in
-            Group {
-                Text(account.name ?? "")
-
-                Button {
-                    if account is BudgetAccount {
-                        viewStore.send(.deleteBudgetAccountRequested(id: account.id))
-                    }
-
-                    if account is TrackingAccount {
-                        viewStore.send(.deleteTrackingAccountRequested(id: account.id))
-                    }
+            VStack {
+                Button(role: .destructive) {
+                    showDeleteAccountConfirmationDialog = true
                 } label: {
                     Text("Delete account")
                 }
+                .confirmationDialog("You are about to delete your account", isPresented: $showDeleteAccountConfirmationDialog, titleVisibility: .visible) {
+                    Button(role: .destructive) {
+                        if account is BudgetAccount {
+                            viewStore.send(.deleteBudgetAccountRequested(id: account.id))
+                        }
+
+                        if account is TrackingAccount {
+                            viewStore.send(.deleteTrackingAccountRequested(id: account.id))
+                        }
+                    } label: {
+                        Text("Delete")
+                    }
+                } message: {
+                    Text("This action cannot be undone")
+                }
+
             }
                 .navigationTitle(account.name ?? "")
                 .navigationViewStyle(.columns)
