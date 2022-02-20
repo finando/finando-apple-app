c406815b7b20b2a22872a35474bafb40fc39b546
Alexander Zakharov <alzak@envisage.no>
2022-02-20 11:11:24 +0100

    feat: add forms for each type of transaction

--- Finando/iOS/Views/NewTransactionModalView+iOS.swift
+++ Finando/iOS/Views/NewTransactionModalView+iOS.swift
@@ -1,14 +1,60 @@
 import SwiftUI
+import ComposableArchitecture
 
 struct NewTransactionModalView: View {
     let store: Store<AccountsState, AccountsAction>
 
     @Environment(\.dismiss) private var dismiss: DismissAction
 
+    @State private var transactionType = TransactionType.expense
+
     var body: some View {
-        NavigationView {
-            Text("HELLO")
+        WithViewStore(store) { viewStore in
+            NavigationView {
+                VStack(spacing: 16) {
+                    Picker("Transaction type", selection: $transactionType) {
+                        ForEach(TransactionType.allCases, id: \.self) { transactionType in
+                            Text(transactionType.translation)
+                                .tag(transactionType)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    .headerProminence(.increased)
+                    .padding(.horizontal, 16)
+
+                    TabView(selection: $transactionType) {
+                        NewIncomeTransactionFormView()
+                            .tag(TransactionType.income)
+
+                        NewExpenseTransactionFormView(accounts: viewStore.accounts)
+                            .tag(TransactionType.expense)
+
+                        NewTransferTransactionFormView()
+                            .tag(TransactionType.transfer)
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+
+                    Button {
+                        print("SUBMIT")
+                    } label: {
+                        Label("Create transaction", systemImage: "paperplane.fill")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .padding(.vertical, 16)
                 .navigationTitle("New transaction")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text("Cancel")
+                        }
+
+                    }
+                }
+
+            }
         }
     }
 }
@@ -16,51 +62,47 @@ struct NewTransactionModalView: View {
 struct NewTransactionModalView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ZStack {
-                NewTransactionModalView(
-                    store: Store(
-                        initialState: AccountsState(
-                            accounts: [
-                                BudgetAccount(id: "demo-account-id-1", name: "Demo account 1"),
-                                BudgetAccount(id: "demo-account-id-2", name: "Demo account 2"),
-                                BudgetAccount(id: "demo-account-id-3", name: "Demo account 3"),
-                                BudgetAccount(id: "demo-account-id-4", name: "Demo account 4"),
-                                BudgetAccount(id: "demo-account-id-5", name: "Demo account 5")
-                            ]
-                        ),
-                        reducer: accountsReducer,
-                        environment: AccountsEnvironment(
-                            mainQueue: .main,
-                            accountService: AccountService(apolloClient: Network.shared.apollo)
-                        )
+            NewTransactionModalView(
+                store: Store(
+                    initialState: AccountsState(
+                        accounts: [
+                            BudgetAccount(id: "demo-account-id-1", name: "Demo account 1"),
+                            BudgetAccount(id: "demo-account-id-2", name: "Demo account 2"),
+                            BudgetAccount(id: "demo-account-id-3", name: "Demo account 3"),
+                            BudgetAccount(id: "demo-account-id-4", name: "Demo account 4"),
+                            BudgetAccount(id: "demo-account-id-5", name: "Demo account 5")
+                        ]
+                    ),
+                    reducer: accountsReducer,
+                    environment: AccountsEnvironment(
+                        mainQueue: .main,
+                        accountService: AccountService(apolloClient: Network.shared.apollo)
                     )
                 )
-            }
-            .preferredColorScheme(.light)
-            .previewDisplayName("Light mode")
+            )
+                .preferredColorScheme(.light)
+                .previewDisplayName("Light mode")
 
-            ZStack {
-                NewTransactionModalView(
-                    store: Store(
-                        initialState: AccountsState(
-                            accounts: [
-                                BudgetAccount(id: "demo-account-id-1", name: "Demo account 1"),
-                                BudgetAccount(id: "demo-account-id-2", name: "Demo account 2"),
-                                BudgetAccount(id: "demo-account-id-3", name: "Demo account 3"),
-                                BudgetAccount(id: "demo-account-id-4", name: "Demo account 4"),
-                                BudgetAccount(id: "demo-account-id-5", name: "Demo account 5")
-                            ]
-                        ),
-                        reducer: accountsReducer,
-                        environment: AccountsEnvironment(
-                            mainQueue: .main,
-                            accountService: AccountService(apolloClient: Network.shared.apollo)
-                        )
+            NewTransactionModalView(
+                store: Store(
+                    initialState: AccountsState(
+                        accounts: [
+                            BudgetAccount(id: "demo-account-id-1", name: "Demo account 1"),
+                            BudgetAccount(id: "demo-account-id-2", name: "Demo account 2"),
+                            BudgetAccount(id: "demo-account-id-3", name: "Demo account 3"),
+                            BudgetAccount(id: "demo-account-id-4", name: "Demo account 4"),
+                            BudgetAccount(id: "demo-account-id-5", name: "Demo account 5")
+                        ]
+                    ),
+                    reducer: accountsReducer,
+                    environment: AccountsEnvironment(
+                        mainQueue: .main,
+                        accountService: AccountService(apolloClient: Network.shared.apollo)
                     )
                 )
-            }
-            .preferredColorScheme(.dark)
-            .previewDisplayName("Dark mode")
+            )
+                .preferredColorScheme(.dark)
+                .previewDisplayName("Dark mode")
         }
     }
 }
