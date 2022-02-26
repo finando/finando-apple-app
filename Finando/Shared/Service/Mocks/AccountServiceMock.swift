19590817b1cef4c8377bbf4f443df842198d3b1e
Alexander Zakharov <alzak@envisage.no>
2022-02-26 21:14:41 +0100

    test: add service mocks

---
+++ Finando/Shared/Service/Mocks/AccountServiceMock.swift
@@ -0,0 +1,31 @@
+final class AccountServiceMock: AccountServiceProtocol {
+    let accounts: [Account]
+
+    init(accounts: [Account] = []) {
+        self.accounts = accounts
+    }
+
+    func listAccounts() async -> [Account] {
+        return []
+    }
+
+    func getAccount(id: String) async -> Account? {
+        return nil
+    }
+
+    func createBudgetAccount(data: CreateBudgetAccountInput) async -> Account? {
+        return nil
+    }
+
+    func createTrackingAccount(data: CreateTrackingAccountInput) async -> Account? {
+        return nil
+    }
+
+    func deleteBudgetAccount(id: String) async -> Account? {
+        return nil
+    }
+
+    func deleteTrackingAccount(id: String) async -> Account? {
+        return nil
+    }
+}
---
+++ Finando/Shared/Service/Mocks/TransactionServiceMock.swift
@@ -0,0 +1,19 @@
+final class TransactionServiceMock: TransactionServiceProtocol {
+    let transactions: [Transaction]
+
+    init(transactions: [Transaction] = []) {
+        self.transactions = transactions
+    }
+
+    func listTransactions(accountId: String) async -> [Transaction] {
+        return transactions
+    }
+
+    func listLatestTransactions(accountId: String, pagination: Pagination) async -> ([Transaction], Bool) {
+        return (transactions, false)
+    }
+
+    func listScheduledTransactions(accountId: String) async -> [ScheduledTransaction] {
+        return []
+    }
+}
