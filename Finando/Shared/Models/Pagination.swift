dda9024f7c1c3feacea26a99de50c218c42e599d
Alexander Zakharov <alzak@envisage.no>
2022-02-26 18:59:44 +0100

    fix: make Pagination properties optional

--- Finando/Shared/Models/Pagination.swift
+++ Finando/Shared/Models/Pagination.swift
@@ -1,4 +1,9 @@
 struct Pagination: Equatable {
-    let cursor: String
-    let take: Int
+    let cursor: String?
+    let take: Int?
+
+    init(cursor: String? = nil, take: Int? = nil) {
+        self.cursor = cursor
+        self.take = take
+    }
 }
