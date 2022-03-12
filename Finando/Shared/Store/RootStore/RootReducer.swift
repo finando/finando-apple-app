0f2559a66f0f57ce1408f16d937ed2cf2a276ae3
Alexander Zakharov <alzak@envisage.no>
2022-03-12 20:53:42 +0100

    refactor: change indentation in root reducer

--- Finando/Shared/Store/RootStore/RootReducer.swift
+++ Finando/Shared/Store/RootStore/RootReducer.swift
@@ -5,36 +5,39 @@ let rootReducer = Reducer<
     RootAction,
     RootEnvironment
 >.combine(
-    accountsReducer.pullback(
-        state: \.accounts,
-        action: /RootAction.accounts,
-        environment: {
-            .init(
-                mainQueue: $0.mainQueue,
-                accountService: AccountService(apolloClient: $0.apolloClient)
-            )
-        }
-    ),
-    accountReducer.pullback(
-        state: \.account,
-        action: /RootAction.account,
-        environment: {
-            .init(
-                mainQueue: $0.mainQueue,
-                accountService: AccountService(apolloClient: $0.apolloClient)
-            )
-        }
-    ),
-    transactionsReducer.pullback(
-        state: \.transactions,
-        action: /RootAction.transactions,
-        environment: {
-            .init(
-                mainQueue: $0.mainQueue,
-                transactionsService: TransactionService(apolloClient: $0.apolloClient)
-            )
-        }
-    )
+    accountsReducer
+        .pullback(
+            state: \.accounts,
+            action: /RootAction.accounts,
+            environment: {
+                .init(
+                    mainQueue: $0.mainQueue,
+                    accountService: AccountService(apolloClient: $0.apolloClient)
+                )
+            }
+        ),
+    accountReducer
+        .pullback(
+            state: \.account,
+            action: /RootAction.account,
+            environment: {
+                .init(
+                    mainQueue: $0.mainQueue,
+                    accountService: AccountService(apolloClient: $0.apolloClient)
+                )
+            }
+        ),
+    transactionsReducer
+        .pullback(
+            state: \.transactions,
+            action: /RootAction.transactions,
+            environment: {
+                .init(
+                    mainQueue: $0.mainQueue,
+                    transactionsService: TransactionService(apolloClient: $0.apolloClient)
+                )
+            }
+        )
 )
 
 extension Reducer {
